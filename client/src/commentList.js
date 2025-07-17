eec7b7f4e9cdccd14f15c854aaf372d7c06d9706
codingbbq <codingbbq@gmail.com>
2025-07-17 15:51:43 +0530

    When Query service is offline

--- client/src/commentList.js
+++ client/src/commentList.js
@@ -3,6 +3,11 @@ import React from "react";
 const CommentList = ({ comments }) => {
 
     const renderComments = comments.map(comment => {
+        
+        // Check if the comment status is 'approved' before rendering
+        if (comment.status !== 'approved') {
+            return null; // Skip rendering for non-approved comments
+        }
         return (
             <li key={comment.id}>
                 {comment.content}
